6967688517965e2b3a1b3083e8bf6212f92c6d4b
leo5295 <leonardo.martellosio@gmail.com>
2023-02-20 21:55:03 +0100

    fix col

--- resources/views/partials/series.blade.php
+++ resources/views/partials/series.blade.php
@@ -2,14 +2,14 @@
     <div class="container-fluid">
         <div class="row">
             @foreach ($series as $key => $item)
-            <a href="{{route('single', ['param' => $key])}}">
             <div class="col-2">
+            <a href="{{route('single', ['param' => $key])}}">
                 <img src="{{$item['thumb']}}" alt="{{$item['title']}}">
                 <div class="title">
                     {{$item['series']}}
                 </div>
-            </div>
             </a>
+            </div>
             @endforeach
         </div>
     </div>
