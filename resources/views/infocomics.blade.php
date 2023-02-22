b1c1de18bc226979df5334ccbf89219da06e5d4f
leo5295 <leonardo.martellosio@gmail.com>
2023-02-22 14:59:09 +0100

    some style infocomics

--- resources/views/infocomics.blade.php
+++ resources/views/infocomics.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-2">
-    <img src="{{$single_comic['thumb']}}" alt="{{$single_comic['title']}}">
-    <div class="title">
-        {{$single_comic['series']}}
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="{{$single_comic['thumb']}}" alt="{{$single_comic['title']}}">
+            <h1 class="title">
+                {{$single_comic['series']}}
+            </h1>
+            <div class="description">
+                {{$single_comic['description']}}
+            </div>
+            <div class="price">
+                Price: {{$single_comic['price']}}
+            </div>
+        </div>
     </div>
 </div>
 @endsection
